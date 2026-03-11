2e150280f274c5342373c9d6216349dce1d7cf33
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-03-11 15:51:01 +0100

    router

--- src/web/router.rs
+++ src/web/router.rs
@@ -1,26 +1,42 @@
-use std::{fs, io::{BufRead, BufReader, Write}, net::TcpStream};
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+};
 
-pub struct Router{
-}
+pub struct Router {}
 
-pub fn handle_connection(folder : String,mut stream: TcpStream) -> () {
+pub fn handle_connection(folder: String, mut stream: TcpStream) -> () {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-    
-    
-    let contents = fs::read_to_string(folder+"/"+filename).unwrap();
-    let length = contents.len();
+    let mut req = request_line.split(" ");
 
+    let request_type: &str = req.next().unwrap_or_else(|| "wrong type");
+    let mut filename: String = req.next().unwrap_or_else(|| "404.html").to_string();
+
+    let status_line: &str;
+    match request_type {
+        "GET" => {
+            status_line = "HTTP/1.1 200 OK";
+        }
+        _ => {
+            status_line = "HTTP/1.1 405 Method Not Allowed";
+            filename = "405.html".to_string();
+        }
+    };
+    if filename.chars().last().unwrap() == '/' {
+        filename += "index.html";
+    }else{
+        filename += ".html"
+    }
+
+    let contents = fs::read_to_string(folder + filename.as_str())
+        .unwrap_or_else(|_| "No content file somehow".to_string());
+
+    let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
 
-fn uknown_route() -> (){
-
-}
+fn uknown_route() -> () {}
